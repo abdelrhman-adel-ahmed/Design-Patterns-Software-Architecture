9be6ba8b00c57a7ff359efc75b803cbd3ab67dca
abdelrhman-adel-ahmed <abdelrhman.bedo007@gmail.com>
2021-08-27 06:35:30 +0200

    Update coupling.py

--- Cohesion and coupling/coupling.py
+++ Cohesion and coupling/coupling.py
@@ -1,8 +1,8 @@
 class Validate:
-    
-#here the function is tight coupled with the mail object implementation so if the email object 
-#structre changes we will need to change the function here also, the solution is directly send what 
-#we need through indirection layer (helper method)
+
+    # here the function is tight coupled with the mail object implementation so if the email object
+    # structre changes we will need to change the function here also the solution is directly send what
+    # we need through indirection layer
     def validate_email(email):
         if email.headr[...] == ...:
             pass
@@ -10,21 +10,27 @@ class Validate:
             pass
 
 
-#-----------------------------
+# -----------------------------
+
 
 class email:
-    def _validate_email(self):
-        """the logic goes here"""
+    email: str
+    var1: str
+    var2: str
+
+    def _validate_email(self) -> bool:
+        """the logic go here"""
+        self.var1
+        self.var2
 
     def email_validator(self):
         """
-        another indirection layer we will use inside the class it self so if the implmentation of validate_email changes we will not 
-        need to change any thing in the other classes that uses it ,another good way is to not expose the implementation of the course this is python 
-        but who cares :) !
+        indirection layer we will use so if the implmentation of validate_email changes we will not
+        need to change any thing in the other classes that use that class
         """
         self._validate_email()
 
 
-class Validate2():
+class Validate2:
     def validate_email(email):
-      email.email_validator()
+        email.email_validator()
